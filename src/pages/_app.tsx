7f6ffb0a85727a9712481cb5fb763c85feae166d
uncox-ali <alif.mohamady20@gmail.com>
2023-09-10 22:10:50 +0330

    fixed save message to localstorage and add scroll to new message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { store } from "@/store";
+import "@/styles/globals.css";
+import { Box } from "@mui/material";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import { Provider } from "react-redux";
+
+const MainNoSSR = dynamic(() => import("@/components/MainNoSSR"), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <MainNoSSR>
+        <Component {...pageProps} />
+      </MainNoSSR>
+    </Provider>
+  );
 }
